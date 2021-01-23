7133a120326926784aa571a78015470a95ebeb75
gzdkoca <73363774+gzdkoca@users.noreply.github.com>
2021-01-23 23:53:24 +0300

    23 Ocak 2021 ders sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //constructor --> ctor - tabx2
+        public MyList()
+        {
+            items = new T[0];
+        }
+
+        public void Add(T item)  //yeni gelen eleman
+        {
+            T[] tempArray = items;
+            items = new T[items.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;  // yeni gelen elemanı sona ekler.
+        }
+    }
+}
